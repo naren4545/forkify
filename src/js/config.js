384422aa390abc10cb4d6a0c2be5fdfed7073eda
naren4545 <narenchavan26@gmail.com>
2024-01-07 16:57:36 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '<c4c6c4bd-c57e-4383-b68b-fc6bc4d7f972>'
+export const MODAL_CLOSE_SEC = 2.5;
